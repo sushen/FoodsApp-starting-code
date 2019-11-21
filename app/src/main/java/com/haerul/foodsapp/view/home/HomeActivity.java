c550dcfbb95d202d7860e636dcf984f41dd8bdae
sushen <sushenbiswasaga@gmail.com>
2019-11-21 23:45:26 +0600

    Chapere 1 Complite.

--- app/src/main/java/com/haerul/foodsapp/view/home/HomeActivity.java
+++ app/src/main/java/com/haerul/foodsapp/view/home/HomeActivity.java
@@ -6,7 +6,7 @@
  -----------------------------------------------------------------------------*/
 package com.haerul.foodsapp.view.home;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +15,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
 
+
 import com.haerul.foodsapp.R;
 import com.haerul.foodsapp.Utils;
 import com.haerul.foodsapp.adapter.RecyclerViewHomeAdapter;
@@ -22,40 +23,78 @@ import com.haerul.foodsapp.adapter.ViewPagerHeaderAdapter;
 import com.haerul.foodsapp.model.Categories;
 import com.haerul.foodsapp.model.Meals;
 
+
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-// TODO 31 implement the HomeView interface at the end
-public class HomeActivity extends AppCompatActivity {
 
-    /*
-     * TODO 32 Add @BindView Annotation (1)
-     *
-     * 1. viewPagerHeader
-     * 2. recyclerCategory
-     *
-     */
+public class HomeActivity extends AppCompatActivity implements HomeView {
 
-    /*
-     *  TODO 33 Create variable for presenter;
-     */
+        @BindView(R.id.viewPagerHeader) ViewPager viewPagerMeal;
+        @BindView(R.id.recyclerCategory) RecyclerView recyclerViewCategory;
+
+        HomePresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        /*
-         *  TODO 34 bind the ButterKnife (2)
-         */
+        ButterKnife.bind(this);
 
-        /*
-         *  TODO 35 Declare the presenter
-         *  new HomePresenter(this)
-         */
+        presenter = new HomePresenter(this);
+        presenter.getMeals();
+        presenter.getCategories();
+
     }
 
-    // TODO 36 Overriding the interface
+    @Override
+    public void showLoading() {
+        findViewById(R.id.shimmerMeal).setVisibility(View.VISIBLE);
+        findViewById(R.id.shimmerCategory).setVisibility(View.VISIBLE);
+    }
 
+    @Override
+    public void hideLoading() {
+        findViewById(R.id.shimmerMeal).setVisibility(View.GONE);
+        findViewById(R.id.shimmerCategory).setVisibility(View.GONE);
+    }
+
+    @Override
+    public void setMeal(List<Meals.Meal> meal) {
+        ViewPagerHeaderAdapter headerAdapter = new ViewPagerHeaderAdapter(meal,this);
+
+//        TODO IN TUTORIALS HE SHOW viewPagerMeal
+
+        viewPagerMeal.setAdapter(headerAdapter);
+        viewPagerMeal.setPadding(20,0,150,0);
+        headerAdapter.notifyDataSetChanged();
+
+        headerAdapter.setOnItemClickListener((v, position) -> {
+            Toast.makeText(this,meal.get(position).getStrMeal(), Toast.LENGTH_SHORT).show();
+        });
+
+    }
+
+    @Override
+    public void setCategory(List<Categories.Category> category) {
+        RecyclerViewHomeAdapter homeAdapter = new RecyclerViewHomeAdapter(category,this);
+        recyclerViewCategory.setAdapter(homeAdapter);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,3
+                ,GridLayoutManager.VERTICAL,false);
+        recyclerViewCategory.setLayoutManager(layoutManager);
+        recyclerViewCategory.setNestedScrollingEnabled(true);
+        homeAdapter.notifyDataSetChanged();
+
+        homeAdapter.setOnItemClickListener((view, position) -> {
+            Toast.makeText(this,category.get(position).getStrCategory(),Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public void onErrorLoading(String message) {
+        Utils.showDialogMessage(this,"Title",message);
+
+    }
 }
